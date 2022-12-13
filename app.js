728ada7b8295687e912374d473bce2ba7cb992ec
Bukabuka321 <20uva02@gmail.com>
2022-12-13 12:42:20 +0100

    done quest 08

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.APP_PORT ?? 3000;
 
 app.use(express.json());
 
-const {hashPassword} = require("./auth")
+const {hashPassword, getUserByEmailWithpassowrdAndPassToNext, verifyPassword, verifyToken} = require("./auth")
 
 
 const userHandlers = require("./userHandlers");
@@ -24,12 +24,22 @@ app.get("/", welcome);
 
 // const movieHandlers = require("./movieHandlers");
 
-
-app.get("/api/users/:id", userHandlers.getUsersById);
+app.post(
+  "/api/login",
+  getUserByEmailWithpassowrdAndPassToNext,
+  verifyPassword
+);
 app.post( "/api/users", hashPassword, userHandlers.postUser);
+app.get("/api/users/:id", userHandlers.getUsersById);
+app.get("/api/users", userHandlers.getUsersByParams);
+
+app.use(verifyToken);
+
 app.put("/api/users/:id",userHandlers.validateUser, userHandlers.putUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
-app.get("/api/users", userHandlers.getUsersByParams);
+
+
+
 
 
 app.listen(port, (err) => {
--- auth.js
+++ auth.js
@@ -1,5 +1,9 @@
 const argon2 = require("argon2");
 
+const database = require("./database");
+
+const jwt = require("jsonwebtoken")
+
 const hashParams = {
     type: argon2.argon2id,
     memoryCost: 2 ** 16,
@@ -24,6 +28,75 @@ const hashPassword = (req, res, next) => {
 }
 
 
+const getUserByEmailWithpassowrdAndPassToNext = (req, res, next) => {
+    const {email} = req.body;
+    database
+        .query("SELECT * FROM users WHERE email = ?", [email])
+        .then(([users]) => {
+            if(users[0] != null) {
+                req.user = users[0];
+
+                next();
+            } else {
+                res.sendStatus(401)
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error retrieving data from database");
+        });
+};
+
+const verifyPassword = (req, res) => {
+    argon2
+        .verify(req.user.hashedPassword, req.body.password)
+        .then((isVerified) => {
+            if(isVerified) {
+                const payload = { sub: req.user.id };
+
+                const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                    expiresIn: "1h",
+                });
+
+                delete req.user.hashedPassword;
+                res.send({token, user: req.user});
+            } else {
+                res.sendStatus(401);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+        })
+}
+
+const verifyToken = (req, res, next) => {
+    try {
+      const authorizationHeader = req.get("Authorization");
+  
+      if (authorizationHeader == null) {
+        throw new Error("Authorization header is missing");
+      }
+  
+      const [type, token] = authorizationHeader.split(" ");
+  
+      if (type !== "Bearer") {
+        throw new Error("Authorization header has not the 'Bearer' type");
+      }
+  
+      req.payload = jwt.verify(token, process.env.JWT_SECRET);
+  
+      next();
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(401);
+    }
+  };
+  
+
 module.exports = {
-    hashPassword
+    hashPassword,
+    getUserByEmailWithpassowrdAndPassToNext,
+    verifyPassword,
+    verifyToken
 };
